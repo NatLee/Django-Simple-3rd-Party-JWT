1cdc06d900820e89ea97ed42e0c4321b20c88209
NatLee <natlee.work@gmail.com>
2024-01-12 14:31:55 +0800

    [update] adjust frontend example

--- example/django_simple_third_party_jwt_example/dev_dashboard/static/js/login.js
+++ example/django_simple_third_party_jwt_example/dev_dashboard/static/js/login.js
@@ -1,15 +1,31 @@
 $("form").on("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
+  let action = $(this).find("button[type=submit]:focus").val();
+
+  if (action === 'jwt') {
+    // JWT Login logic
     $.ajax({
-        type: "POST",
-        url: "/api/auth/token",
-        data: $(this).serialize(),
-        success: function (data) {
-          console.log(data);
-          localStorage.setItem('refresh_token', data.refresh);
-          localStorage.setItem('access_token', data.access);
-          window.location.href = "/api/__hidden_dev_dashboard";
-        }
+      type: "POST",
+      url: "/api/auth/token",
+      data: $(this).serialize(),
+      success: function (data) {
+        console.log(data);
+        localStorage.setItem('refresh_token', data.refresh);
+        localStorage.setItem('access_token', data.access);
+        window.location.href = "/api/__hidden_dev_dashboard";
+      }
     });
+  } else if (action === 'session') {
+    // Session Login logic
+    $.ajax({
+      type: "POST",
+      url: "/api/__hidden_dev_dashboard/login",
+      data: $(this).serialize(),
+      success: function (data) {
+        console.log(data);
+        window.location.href = "/api/__hidden_dev_dashboard";
+      }
+    });
+  }
 });
